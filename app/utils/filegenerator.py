dc31630d44ee2c49c19ae520b3ba1ba83a82e747
Jun He <junnhe@gmail.com>
2013-03-31 07:47:35 -0700

    now use SN number to name file. for easy operations

--- app/utils/filegenerator.py
+++ app/utils/filegenerator.py
@@ -4,23 +4,27 @@ import itertools,subprocess
 import sys
 from time import gmtime, strftime
 
-filecounts = [100, 100, 100, 100, 10, 1]
-filesizes = [1, 10, 100, 1024, 10240, 102400] # KB
+filecounts = [100, 100, 100, 100,  10,    1]
+filesizes =  [1,   10,  100, 1024, 10240, 102400] # KB
 
 rndline = "asdjkl4321\n" # reason to use a psudo random string is that 
                          # generating too many random numbers is time
                          # consuming.
-
-for i in range(0, 5):
+sn = 0
+testdir = "testfiles/"
+for i in range(0, 6):
     cnt = filecounts[i]
     sz = filesizes[i]
     print cnt, sz,
 
     nline = sz*1024/11
     for j in range(0, cnt):
-        fname = str(sz).zfill(4)+"."+str(j).zfill(4)+".txt"
+        #fname = str(sz).zfill(4)+"."+str(j).zfill(4)+".txt"
+        fname = testdir+str(sn)+".txt"
+        sn = sn + 1
         print "Writing", fname, "..."
         f = open(fname, "w")
         for k in range(0, nline):
             f.write(rndline)
+
 
