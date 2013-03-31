3375efbe59282075f62aa8a0240b38c9fb9bbf73
Jun He <junnhe@gmail.com>
2013-03-31 00:22:38 -0700

    added filegenerator.py

---
+++ app/utils/filegenerator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import itertools,subprocess
+import sys
+from time import gmtime, strftime
+
+filecounts = [100, 100, 100, 100, 10, 1]
+filesizes = [1, 10, 100, 1024, 10240, 102400] # KB
+
+rndline = "asdjkl4321\n" # reason to use a psudo random string is that 
+                         # generating too many random numbers is time
+                         # consuming.
+
+for i in range(0, 5):
+    cnt = filecounts[i]
+    sz = filesizes[i]
+    print cnt, sz,
+
+    nline = sz*1024/11
+    for j in range(0, cnt):
+        fname = str(sz).zfill(4)+"."+str(j).zfill(4)+".txt"
+        print "Writing", fname, "..."
+        f = open(fname, "w")
+        for k in range(0, nline):
+            f.write(rndline)
+
